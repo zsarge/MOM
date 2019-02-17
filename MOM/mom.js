9f5d13358b5b2b226ed5003eb73030732d563448
Zack Sargent <46602241+ZacharyWSargent@users.noreply.github.com>
2019-02-16 21:09:02 -0500

    Uploaded Version 1.0

---
+++ MOM/mom.js
@@ -0,0 +1,4 @@
+//Note: this file used to contain 100+ lines of code.
+//I changed the way I was trying to block sites, and this is all I need now.
+console.log("This site has been blocked.")
+window.location.replace("https://zacharywsargent.github.io/"); //this is currently a landing page saying that the page has been blocked.
